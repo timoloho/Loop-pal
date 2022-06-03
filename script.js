a2ca571833c5c7343dc53af639259e64a44b9417
FluffyQuake <93243216+FluffyQuake@users.noreply.github.com>
2022-06-04 01:04:07 +0300

    Dropdown upadate :))

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches(".link")
+    if (!isDropdownButton && e.target.closest('.dropdown') != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('.dropdown')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll(".dropdown.active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
